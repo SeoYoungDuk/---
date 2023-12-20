513765aa6a5a3db6cd44c806b47ade9c8233aaba
YoungDukSeo <syd9538@tradlinx.com>
2023-12-20 09:24:38 +0900

    feat: 필드 동기화 개발, 적용, 동시성 문제

---
+++ src/main/java/com/hello/advanced/LogTraceConfig.java
@@ -0,0 +1,15 @@
+package com.hello.advanced;
+
+import com.hello.advanced.trace.logtrace.FieldLogTrace;
+import com.hello.advanced.trace.logtrace.LogTrace;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LogTraceConfig {
+
+    @Bean
+    public LogTrace logTrace() {
+        return new FieldLogTrace();
+    }
+}
